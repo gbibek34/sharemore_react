d706246b3582da4199d702d7fe641a10e953fcd2
gbibek34 <gbibek34@gmail.com>
2022-01-29 20:31:51 +0545

    Setup actions for context api

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
